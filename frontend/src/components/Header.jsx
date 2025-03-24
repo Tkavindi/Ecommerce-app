8942033b689fe73e36aef4c473549afeccb4fc09
Tkavindi <tharushak77@gmail.com>
2025-03-24 20:25:54 +0530

    Initialized repository

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.svg'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        <div>
+            {/* logo */}
+            <div>
+                <Link><img src={logo} alt="" height={66} width={66}/></Link>
+            </div>
+
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default Header
