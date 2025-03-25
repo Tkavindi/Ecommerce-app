f22aa70881a9acf67ce25ba90800576b3f232089
Tkavindi <tharushak77@gmail.com>
2025-03-25 18:42:42 +0530

    Offer.jsx created

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -10,7 +10,7 @@ const Item = ({ id, name, image, new_price, old_price }) => {
           to={`product/${id}`}
           className="h-12 w-12 bg-white rounded-full flexCenter absolute top-1/2 
           bottom-1/2 !py-2 z-20 scale-0 group-hover:scale-100 transition-all duration-700 "
-        ><FaSearch className="hover:rotate-90 hover:scale-125 transition-all duration-200"/></Link>
+        ><FaSearch className="scale-125 hover:rotate-90 transition-all duration-200"/></Link>
         <img src={image} alt="productImage" className="w-full block object-cover group-hover:scale-110
         transition-all duration-1000" />
       </div>
